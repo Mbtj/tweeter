a092d563c98c505ee4e144a74ed2a8261ea84e6e
TJ <michael.btj@hotmail.com>
2021-12-15 18:39:03 +0000

    add counter script

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+  console.log("Tweeter Ready");
+  
+  $('textarea').keyup(() => {
+    let textLength = $('textarea').val().length;
+    let counter = $('.counter');
+    let textRemaining = 140 - textLength;
+
+    counter.text(textRemaining);
+    if (textRemaining < 0) {
+      counter.css('color','red');
+  } else {
+    counter.css('color', $('body').prop('color'));
+  }
+  });
+
+  // $('textarea').blur(() => {
+  //   console.log('blur')
+  // });
+  
+  // $('textarea').keydown(() => {
+  //   console.log('keypress')
+  // });
+
+  // $('textarea').change(() => {
+  //   console.log('change')
+  // });
+
+  // $('textarea').input(() => {
+  //   console.log('input')
+  // });
+});
+
