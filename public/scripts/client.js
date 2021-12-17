ee92c17e2369aa9df80d772548be3eafc3337122
TJ <michael.btj@hotmail.com>
2021-12-17 18:17:20 +0000

    implement ajax to access tweets db

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,22 +3,87 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+// Fake data taken from initial-tweets.json
 
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
       "handle": "@SirIsaac"
     },
-  "content": {
+    "content": {
       "text": "If I have seen further it is by standing on the shoulders of giants"
     },
-  "created_at": 1461116232227
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
+const renderTweets = function(tweets) {
+// loops through tweets
+// calls createTweetElement for each tweet
+// takes return value and appends it to the tweets container
+  for (const tweet of tweets) {
+    let $tweet = createTweetElement(tweet);
+    $('#tweets-container').append($tweet);
+  }
 }
 
+const createTweetElement = function(tweet) {
+let $tweet = $(`
+<article class="tweet">
+  <header>
+    <span>
+      <img src="${tweet.user.avatars}"><h4>${tweet.user.name}</h4>
+    </span>
+    <div class="tweet-handle">
+      ${tweet.user.handle}
+    </div>
+  </header>
+  <h3>
+    ${tweet.content.text}
+  </h3>
+  <footer>
+    <span>
+      ${timeago.format(tweet.created_at)}
+    </span>
+    <aside class="tweet-buttons">
+      <i class="fa-solid fa-flag"></i>
+      <i class="fa-solid fa-retweet"></i>
+      <i class="fa-solid fa-heart"></i>
+    </aside>
+  </footer>
+</article>
+`);
 
-const $tweet = $(`<article class="tweet">Hello world</article>`);
+return $tweet;
+}
 
-// Test / driver code (temporary)
-console.log($tweet); // to see what it looks like
-$('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+const $button = $('#send-tweet');
+
+$button.submit((event) => {
+  alert('it works');
+  event.preventDefault();
+  const tweetText = $button.serialize();
+  $.post('/tweets', tweetText, () => {
+    console.log(tweetText);
+  });
+});
+
+
+const loadTweets = function() {
+  $.get('/tweets', (data) => {
+    renderTweets(data);
+  })
+}
+loadTweets(data);
